33ca5edd5285483ed5275af1d9917225cdd309c1
Erich Sickert <esickert@gmail.com>
2016-12-24 15:43:15 -0800

    more

--- project01/src/test/java/com/esickert/chap005testwithourownclasses/domainobject/examples/TestAppEnviromentTest.java
+++ project01/src/test/java/com/esickert/chap005testwithourownclasses/domainobject/examples/TestAppEnviromentTest.java
@@ -3,13 +3,26 @@ package com.esickert.chap005testwithourownclasses.domainobject.examples;
 import com.esickert.domainobject.TestAppEnv;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by esickert on 12/19/2016.
  */
 public class TestAppEnviromentTest {  //remember this in package domainobject.examples
     @Test
     public void getUrlStatistically(){
-        TestAppEnv.getUrl();  //sooo this is a class.method.
+        TestAppEnv.getUrl();  //TedtAppEnv is a class not object (instance) and getUrl is a static method of it.
     }
 
+    @Test
+    public void canGetDomainAndPortStaticallly()    {
+
+        assertEquals("Just the domain:",
+                "192.123.0.3",
+                TestAppEnv.DOMAIN);
+
+        assertEquals("Just the port:",
+                "67",
+                TestAppEnv.PORT);
+    }
 }
