89b39d446107496324f584096bde7b23dbe7b0c5
Erich Sickert <esickert@gmail.com>
2016-12-25 13:22:21 -0800

    more

--- project01/src/main/java/com/esickert/domainobject/TestAppEnv.java
+++ project01/src/main/java/com/esickert/domainobject/TestAppEnv.java
@@ -4,9 +4,13 @@ package com.esickert.domainobject;
  * Created by esickert on 12/19/2016.
  */
 public class TestAppEnv {
+    public static final String DOMAIN = "192.123.0.3";
+    public static final String PORT = "67";
+
     public static String getUrl() {   //the highlighted classes on the left mean they are not commited to github yet.
-        return null; //this does NOT cause an error to occur when using null!!
+        return "http://" + DOMAIN + ":" + PORT; //this does NOT cause an error to occur when using null!!
     }
 }
 //this is very cool in that we call the class name, NOT an instance, followed by the method.
 // We didn't need to create an instance of the class because it's static.
+//NOTE also that we need to import the package into the test package
--- project01/src/test/java/com/esickert/chap005testwithourownclasses/domainobject/examples/TestAppEnviromentTest.java
+++ project01/src/test/java/com/esickert/chap005testwithourownclasses/domainobject/examples/TestAppEnviromentTest.java
@@ -1,26 +1,31 @@
 package com.esickert.chap005testwithourownclasses.domainobject.examples;
 
-import com.esickert.domainobject.TestAppEnv;   //WE ARE IMPORTING FROM AN OUTSIDE PACKAGE
+import com.esickert.domainobject.TestAppEnv;    //WE ARE IMPORTING FROM AN OUTSIDE PACKAGE. STATIC USAGE
+import static com.esickert.domainobject.TestAppEnv.DOMAIN;
+import static com.esickert.domainobject.TestAppEnv.getUrl;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import org.junit.*;
 
 /**
  * Created by esickert on 12/19/2016.
  */
 public class TestAppEnviromentTest {  //remember this in package domainobject.examples
     @Test
-    public void getUrlStatistically(){
-        TestAppEnv.getUrl();  //TedtAppEnv is a class not object (instance) and getUrl is a static method of it.
-        System.out.println(TestAppEnv.getUrl());
+    public void canGetUrlStatistically(){
+        getUrl();  //TedtAppEnv is a class not object (instance) and getUrl is a static method of it.
+        System.out.println(getUrl());
     }
 
+    //There are different methods here for coding the same thing. One called Static Import with the other Staic Usage.
     @Test
     public void canGetDomainAndPortStaticallly()    {
-
-        assertEquals("Just the domain:",
+        //Static Import
+        Assert.assertEquals("Just the domain:",  //note the addition of Assert before assertEquals
                 "192.123.0.3",
-                TestAppEnv.DOMAIN);
+                DOMAIN);
 
+        //Static Usage
         assertEquals("Just the port:",
                 "67",
                 TestAppEnv.PORT);
