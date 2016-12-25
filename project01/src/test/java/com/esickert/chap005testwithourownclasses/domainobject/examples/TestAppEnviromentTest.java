493a95ff4626ce39f20c3bc564c7f115517b8c13
Erich Sickert <esickert@gmail.com>
2016-12-24 16:29:46 -0800

    more

--- project01/src/test/java/com/esickert/chap005testwithourownclasses/domainobject/examples/TestAppEnviromentTest.java
+++ project01/src/test/java/com/esickert/chap005testwithourownclasses/domainobject/examples/TestAppEnviromentTest.java
@@ -1,8 +1,7 @@
 package com.esickert.chap005testwithourownclasses.domainobject.examples;
 
-import com.esickert.domainobject.TestAppEnv;
+import com.esickert.domainobject.TestAppEnv;   //WE ARE IMPORTING FROM AN OUTSIDE PACKAGE
 import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -12,6 +11,7 @@ public class TestAppEnviromentTest {  //remember this in package domainobject.ex
     @Test
     public void getUrlStatistically(){
         TestAppEnv.getUrl();  //TedtAppEnv is a class not object (instance) and getUrl is a static method of it.
+        System.out.println(TestAppEnv.getUrl());
     }
 
     @Test
